a4f6c616fb791b1af3a1b4aa59e8f22ba1cad333
byfallcode <bayecode5991@gmail.com>
2023-02-14 20:49:59 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+   Route::resource('users', UserController::class);
+
+});
